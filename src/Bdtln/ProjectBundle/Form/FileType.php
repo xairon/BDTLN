c189c6aa2f7fc50064d3aff031c03c351e993046
anthonybocci <boccianthony@yahoo.fr>
2015-04-22 01:50:36 +0200

    Creating of Project and File Form in order to add and update a bundle

---
+++ src/Bdtln/ProjectBundle/Form/FileType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Bdtln\ProjectBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class FileType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('path')
+            ->add('title')
+            ->add('project')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Bdtln\ProjectBundle\Entity\File'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'bdtln_projectbundle_file';
+    }
+}
