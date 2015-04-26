368d09c2510a208c6b401e5bb9b14466080339f4
anthonybocci <boccianthony@yahoo.fr>
2015-04-26 13:19:15 +0200

    Updating add_axe_in_projectAction and creating of update_projectAction

--- src/Bdtln/ProjectBundle/Controller/ProjectController.php
+++ src/Bdtln/ProjectBundle/Controller/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
 {
     /**
      * indexAction display the page of one project
-     * @param string $project_slug
+     * @param string $project
      * @return Response the page of the wanted project
      */
     public function indexAction( Project $project ) {
@@ -34,11 +34,11 @@ class ProjectController extends Controller
         $request = $this->get('request');
         //If it is a validation of form
         if ( $request->getMethod() == "POST" ) {
+            $idAxe = intval($_POST['axe']);
             //Loading of the form
             $projectForm->bind($request);
             //If all the inputs are valids, and wanted axe is differend of 0, save in database
-            if ( $projectForm->isValid() && intval($_POST['axe']) != 0 ) {  
-                $idAxe = intval($_POST['axe']);
+            if ( $projectForm->isValid() && intval($_POST['axe']) != 0 ) {
                 //Compare idAxe with all axes
                 for ( $i = 0; $i < count($axes); $i++ ) {
                     //If the id of wanted axe is valid
@@ -60,7 +60,7 @@ class ProjectController extends Controller
             } else { //If the form is invalid
                 $this->get('session')->getFlashBag()->add('information', 'Le projet n\'a pas pu être enregistré !');
             }
-            if ( empty($project->getAxes()) )
+            if ( intval($idAxe) == 0 )
             $this->get('session')->getFlashBag()->add('information', 'Veuillez ajouter un axe ! !');
 
         }
@@ -78,42 +78,85 @@ class ProjectController extends Controller
         //Verifier si la personne qui regarde cette page est aussi celle qui est responsable du projet
         
         $entityManager = $this->getDoctrine()->getManager();
-        $axes = $entityManager->getRepository('BdtlnAxeBundle:Axe')->findAll();
-     /*
-        $axesOwnByProject = $project->getAxes()->toArray();
-        $axesNotInProject = array();
+        $axeRepository = $entityManager->getRepository('BdtlnAxeBundle:Axe');
+        $axes = $axeRepository->findAllWithProjects();
+        //The axes witch doesn't own this project
+        $axesNotInProject = $axeRepository->findAllNotInProject($project);
         
-        for ( $i = 0; $i < count($axes); $i++ ) {
-            if ( !in_array($axes[$i], $axesOwnByProject) )
-                    $axesNotInProject[] = $axes[$i];
-        }
-      */
-        
-        
-        //If the form is validated
+        //If the form is submit
         if ( $this->get('request')->getMethod() == "POST" ) {
             $idAxe = intval($_POST['axe']);
             //look over all axes in order to test if the given axe is valid
             for ( $i = 0; $i < count($axes); $i++ ) {
                 if ( $idAxe == $axes[$i]->getId() ) {
-                    if ( !in_array($axes[$i], $axesOwnByProject) ) { //If the project is not already in this axe
+                    //If the project is not already in this axe
+                    if ( in_array($axes[$i], $axesNotInProject) ) { 
                         $axes[$i]->addProject($project);
                         $entityManager->persist($project);
                         $entityManager->flush();
+                        $this->get('session')->getFlashBag()->add('information', 'L\'axe a bien été ajouté !');
+                        return $this->redirect( $this->generateUrl('bdtln_project_add_axe_in_project', array('slug' => $project->getSlug())) );
+                    } else { //If axes[$i] doesn't belong to axes witch are not in project
                         $this->get('session')->getFlashBag()->add('information', 'Ce projet a déjà cet axe !');
-
                     }
                     break;
                 }
             }
-            
         }
         
-        return $this->render('BdtlnProjectBundle:Project:add_axe_in_project.html.twig', array('project' => $project, 'axes' => $axes));
+        return $this->render('BdtlnProjectBundle:Project:add_axe_in_project.html.twig', array('project' => $project, 'axes' => $axesNotInProject));
     }
     
     
-    
+    public function update_projectAction ( Project $project ) {
+        
+        //The entityManager will allow to save the project in the database
+        $entityManager = $this->getDoctrine()->getManager();
+
+        //The form
+        $projectForm = $this->createForm(new ProjectType(), $project);
+        $axes = $entityManager->getRepository('BdtlnAxeBundle:Axe')->findAll();
+        
+        $request = $this->get('request');
+        //If it is a validation of form
+        if ( $request->getMethod() == "POST" ) {
+            $idAxe = intval($_POST['axe']);
+            //Loading of the form
+            $projectForm->bind($request);
+            //If all the inputs are valids, and wanted axe is differend of 0, save in database
+            if ( $projectForm->isValid() && intval($_POST['axe']) != 0 ) {
+                //Compare idAxe with all axes
+                for ( $i = 0; $i < count($axes); $i++ ) {
+                    //If the id of wanted axe is valid
+                    if ( $axes[$i]->getId() == $idAxe ) {
+                        //if at least one description is not empty
+                        if (!empty($project->getFrenchDescription()) || !empty($project->getEnglishDescription())) {
+                            $axes[$i]->addProject($project);
+                            $entityManager->persist($axes[$i]);
+                            $entityManager->persist($project);
+                            $entityManager->flush();
+                            //Redirect on the page of new project
+                            return $this->redirect( $this->generateUrl('bdtln_project_display', array('slug' => $project->getSlug())) );
+                       } else { //If all descriptions are empty
+                           $this->get('session')->getFlashBag()->add('information', 'Au moins une description doit être remplie !');
+                           return $this->render('BdtlnProjectBundle:Project:add_project.html.twig', array('form' => $projectForm->createView(), 'axes' => $axes));
+                        }                           
+                    }
+                }       
+            } else { //If the form is invalid
+                $this->get('session')->getFlashBag()->add('information', 'Le projet n\'a pas pu être enregistré !');
+            }
+            if ( intval($idAxe) == 0 )
+            $this->get('session')->getFlashBag()->add('information', 'Veuillez ajouter un axe ! !');
+
+        }
+        
+
+        
+        
+        
+        return $this->render('BdtlnProjectBundle:Project:update_project.html.twig', array('form' => $projectForm->createView(), 'axes' => $axes));
+    }
     
     
     
