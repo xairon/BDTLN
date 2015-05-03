826b727895f513b9cba9c23068aaf7f6dbca4c96
anthonybocci <boccianthony@yahoo.fr>
2015-05-03 20:54:23 +0200

    Added en empty file for Publications fixtures

---
+++ src/Bdtln/PublicationBundle/DataFixtures/ORM/Publications.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Bdtln\PublicationBundle\DataFixtures\ORM\Publications;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Bdtln\PublicationBundle\Entity\Publication;
+
+
+/**
+ * Publications insert fixtures of Publication in database
+ *
+ * @author anthony
+ */
+class Publications implements FixtureInterface {
+
+    public function load(ObjectManager $manager) {
+       
+        
+   
+        
+        
+         
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
