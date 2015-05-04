c041c9c0596cce21080df74e3bf4642247dfebf0
anthonybocci <boccianthony@yahoo.fr>
2015-05-04 15:54:37 +0200

    Added annotations in order to use Doctrine events to set the datePublication and dateLastUpdate

--- src/Bdtln/PublicationBundle/Entity/Publication.php
+++ src/Bdtln/PublicationBundle/Entity/Publication.php
@@ -9,6 +9,7 @@ use Doctrine\ORM\Mapping as ORM;
  *
  * @ORM\Table(name="publication")
  * @ORM\Entity(repositoryClass="Bdtln\PublicationBundle\Entity\PublicationRepository")
+ * @ORM\HasLifecycleCallbacks()
  */
 class Publication
 {
