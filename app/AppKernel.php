87c92264154f8a84c235fa0e5a854b28dacc5d8f
anthonybocci <boccianthony@yahoo.fr>
2015-05-05 18:06:27 +0200

    Added htmlpurifier in registerBundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -28,6 +28,7 @@ class AppKernel extends Kernel
             new JMS\AopBundle\JMSAopBundle(),
             new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
             new JMS\DiExtraBundle\JMSDiExtraBundle($this),
+            new Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
